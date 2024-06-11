f13d2a7d21e9a58cfb6ae581862d98661817f9a2
blakebonasera <blakebonasera@gmail.com>
2024-06-11 16:23:18 -0700

    strating models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    Username = models.CharField(max_length=25)
+    Email = models.EmailField()
+    Password = models.CharField(max_length=20)
+
+class Token(models.Model):
+    id = models.CharField(max)
+    mintSymbol = models.CharField(max_length=10)
+    vsToken = models.CharField(max)
+    price = models.DecimalField(max_digits=13,decimal_places=8)
+
+
